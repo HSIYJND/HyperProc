9b9cc70bca979f8516db4e8b554991c36cfd9ddf
Jane Cowles <jcowles@umn.edu>
2019-10-16 12:51:21 -0500

    Uploading First Batch

---
+++ R/imu_demcombine.R
@@ -0,0 +1,44 @@
+#' Combine DEM file with IMU
+#'
+#' This function combines the imu/frame file with dem to calculate accurate height throughout the flight
+#' @param imu processed imu/frame file
+#' @param demfile name of dem that is within local subfolder
+#' @param latmin latitude of min elevation in imu - typically takeoff point
+#' @param lonmin longitude of min elevation in imu - typically takeoff point
+#' @keywords 
+#' @export
+#' @examples
+#' imu_demcombine()
+
+imu_demcombine<-function(imu=imu.framematch,demfile="DEM 1m RESCUE/dem_1m_m.bil",latMin=latMinIMU,lonMin=lonMinIMU){
+
+#don't need to do this again
+imu.framexy <- imu[,c("Lon","Lat")]
+imu.framesp <- SpatialPointsDataFrame(coords=imu.framexy,data=imu,proj4string = CRS("+init=epsg:32615"))
+# imu.framesp <- spTransform(imu.framesp,"+init=epsg:32615")
+
+dem1m <- readGDAL(paste0(LocalSource,demfile))
+dem1m <- spTransform(dem1m,"+init=epsg:32615")
+dem_rel <- crop(dem1m,extent(imu.framesp)+40)
+class(dem_rel)
+dem_sf <- st_as_sf(dem_rel)
+# st_write(dem_sf,dsn=paste0(LocalSource,"dem_BBFAB.shp"),layer="dem_BBFAB.shp",driver="ESRI Shapefile",delete_layer=TRUE)
+rm(dem1m)
+# dem_rel <- readOGR(paste0(LocalSource,"dem_BBFAB.shp"))
+# dem_rel <- spTransform(dem_rel,"+init=epsg:32615")
+
+rast <- raster()
+extent(rast) <- extent(dem_rel) 
+ncol(rast) <- round((extent(dem_rel)@xmax-extent(dem_rel)@xmin)/2)
+nrow(rast) <- round((extent(dem_rel)@ymax-extent(dem_rel)@ymin)/2)
+
+# And then ... rasterize it! This creates a grid version 
+# of your points using the cells of rast, values from the IP field:
+dem_rast <- rasterize(dem_rel, rast, dem_rel$band1, fun=mean) 
+crs(dem_rast)<-crs(dem_rel)
+minAlt_dem_atminIMU <- raster::extract(dem_rast,SpatialPoints(cbind(lonMin,latMin)),buffer=2,fun=mean,na.rm=T)
+assign("minAlt_dem_atminIMU", minAlt_dem_atminIMU, envir = .GlobalEnv)
+
+return(dem_rast)
+
+}
