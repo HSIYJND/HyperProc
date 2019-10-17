7fc289efc586cc0d3401f66aee041bb17b09c44d
Jane Cowles <jcowles@umn.edu>
2019-10-17 11:02:38 -0500

    update to imu process code

--- R/imu_framecombine.R
+++ R/imu_framecombine.R
@@ -8,15 +8,14 @@
 #' @examples
 #' imu_framecombine()
 
-imu_framecombine <- function(folderloc=paste0(RemoteSenDataLoc,FolderLoc),imu="imu_gps.txt"){
+imu_framecombine <- function(folderloc=paste0(RemoteSenDataLoc,FolderLoc),imu_filename="imu_gps.txt"){
   framelist <- list.files(folderloc,pattern = "frameIndex")
   framematch <- rbindlist(lapply(framelist,function(x){fi<-fread(paste0(folderloc,x))}))
 
 #coords.epsg = "32615" is utm 15n which is what I want here. "4326" is lat lon
 
-imu <- read.delim(paste0(folderloc,"imu_gps.txt"))
-
-imu <- imu[!is.na(imu$Alt),]
+imu <- read.delim(paste0(folderloc,imu_filename))
+if(length(is.na(imu$Alt)>0)){imu <- imu[!is.na(imu$Alt),]}
 imuxy <- imu[,c("Lon","Lat")]
 imusp <- SpatialPointsDataFrame(coords=imuxy,data=imu,proj4string = CRS("+init=epsg:4326")) 
 imusp <- spTransform(imusp,"+init=epsg:32615")
