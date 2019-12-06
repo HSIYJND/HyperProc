95eab2cc5d5cc24a7afd2cf6ca5b46f403025196
Jane Cowles <jcowles@umn.edu>
2019-12-06 10:32:28 -0600

    checking byframe

--- R/byframe_corr.R
+++ R/byframe_corr.R
@@ -12,8 +12,8 @@ byframe_corr <- function(framex,sdf,ProcessedIMU){
    specdfframe <- sdf[sdf$frame==framex,]
     matchimu <- ProcessedIMU[ProcessedIMU$Frame.==framex,]
     # specdfframebypix <- rbindlist(lapply(sort(unique(specdfframe$x)),spacing_fun,matchimu,specdfframe))
-    
-    specdfframe$Lat2 <- base::seq(from=matchimu$Lat+matchimu$TotMidPointCorrectionY-matchimu$YawYOffset,to=matchimu$Lat+matchimu$TotMidPointCorrectionY+matchimu$YawYOffset,length.out=nrow(specdfframe))
+    #changed line 16 to +offset then - offset
+    specdfframe$Lat2 <- base::seq(from=matchimu$Lat+matchimu$TotMidPointCorrectionY+matchimu$YawYOffset,to=matchimu$Lat+matchimu$TotMidPointCorrectionY-matchimu$YawYOffset,length.out=nrow(specdfframe))
     specdfframe$Lon2 <- base::seq(from=matchimu$Lon+matchimu$TotMidPointCorrectionX-matchimu$YawXOffset,to=matchimu$Lon+matchimu$TotMidPointCorrectionX+matchimu$YawXOffset,length.out=nrow(specdfframe))    
     specdfframe$Heading <- matchimu$Heading
     return(specdfframe)
